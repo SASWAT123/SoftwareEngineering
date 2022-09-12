c883644d5574a0e6a90539dbd1de4f215a8fc717
Yugalee <yugalee.patil@gmail.com>
2022-09-12 01:18:33 -0400

    Added code to update x & y list

--- src/main/java/com/se/homeworktwo/Cols.java
+++ src/main/java/com/se/homeworktwo/Cols.java
@@ -1,6 +1,8 @@
 package com.se.homeworktwo;
 import java.util.*;
 
+import javax.lang.model.util.ElementScanner14;
+
 public class Cols {
 
     Map<Integer, String> names;
@@ -15,9 +17,20 @@ public class Cols {
         this.x = new ArrayList<String>();
         this.y = new ArrayList<String>();
         klass=null;
-        for (int c : this.names.keySet()) {
+        for (int c : this.names.keySet()) 
+        {
             String s = this.names.get(c);
-    }
+            //String col;
+            all.add(s);
+            if (!s.endsWith(":"))
+            {
+                if (s.endsWith("+") || s.endsWith("-"))
+                    y.add(s);
+                else
+                    x.add(s);
+                
+            }
 
-    
+        }    
+}
 }
