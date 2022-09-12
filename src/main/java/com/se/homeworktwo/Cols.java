bd2eeec34dbbb12b9e25dfedead85ac24f4b436c
Yugalee <yugalee.patil@gmail.com>
2022-09-12 00:03:30 -0400

    Changed datatype of 'names'

--- src/main/java/com/se/homeworktwo/Cols.java
+++ src/main/java/com/se/homeworktwo/Cols.java
@@ -3,18 +3,20 @@ import java.util.*;
 
 public class Cols {
 
-    Map<String, String> names;
+    Map<Integer, String> names;
     public ArrayList<String> all;
     public ArrayList<String> x;
     public ArrayList<String> y;
     String klass;
 
-    public Cols (Map<String, String> names) {
+    public Cols (Map<Integer, String> names) {
         this.names=names;
         this.all = new ArrayList<String>();
         this.x = new ArrayList<String>();
         this.y = new ArrayList<String>();
         klass=null;
+        for (int c : this.names.keySet()) {
+            String s = this.names.get(c);
     }
 
     
