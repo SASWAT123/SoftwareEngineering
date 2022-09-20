13cbee66e4a6775af1231409bf9c585002f11d93
Yugalee Patil <yugalee.patil@gmail.com>
2022-09-20 16:50:37 -0400

    Conversion of map to string

--- src/main/java/com/se/homeworktwo/Util.java
+++ src/main/java/com/se/homeworktwo/Util.java
@@ -1,5 +1,6 @@
 package com.se.homeworktwo;
 
+import java.util.*;
 
 public class Util {
 
@@ -14,5 +15,15 @@ public class Util {
         }
         return t;
     }
+
+    public static String o(Map<String,String> t){
+
+    StringBuilder mapAsString = new StringBuilder("{");
+    for (String key : t.keySet()) {
+        mapAsString.append(key + ":" + t.get(key) + ", ");
+    }
+    mapAsString.delete(mapAsString.length()-2, mapAsString.length()).append("}");
+    return mapAsString.toString();
     
+    }
 }
